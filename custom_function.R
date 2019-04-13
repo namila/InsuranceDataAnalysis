e6d850252a09ec5d302588e8cf40a9adf2816080
Namila Perera <NamilaP@99x.lk>
2019-04-13 20:10:14 +0530

    type testing added

--- custom_function.R
+++ custom_function.R
@@ -1,19 +1,35 @@
-
-
 my_function = function(dataSet, parameter){
   
-  for(column_name in names(dataSet)){
-    
-    if(is.numeric(dataSet[column_name])){
-      paste(column_name," is quantitative")
+  for(column_name in colnames(dataSet)){
+    data_column = dataSet[[column_name]]
+    no_of_missing_values = sum(is.na(data_column))
+   
+    if(is.numeric(data_column) == TRUE){
+      cat(column_name," is quantitative \n")
+      
+      if(no_of_missing_values > 0){
+        mean_value = mean(data_column, na.rm = TRUE)
+        data_column[is.na(data_column)] = mean_value
+        
+      }
     } else{
-      paste(column_name," is qualititative")
-    }  
+      cat(column_name," is qualititative \n")
+      
+      if(no_of_missing_values > 0){
+        mode_value = mode(data_column)
+        data_column[is.na(data_column)] = mode_value
+      }
+    }
+    cat("Number of missing values", no_of_missing_values, "\n")
   }
   
+  head(dataSet)
 }
 
-my_function(insuranceData, "")
-
-
+head(SLPopulation)
+my_function(SLPopulation, "")
+is.na(insuranceData$district)
+insuranceData$gender[is.na(insuranceData$gender)]
+sum(is.na(insuranceData[["age"]]))
+cat("asd","ss")
 
